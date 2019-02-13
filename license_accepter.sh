0de258eec1b4602dab2e393b5c0dec0a211f838a
Andrew Kelly <andrew.kelly@bilue.com.au>
2019-02-13 13:48:04 +1100

    Add new license hash.

--- license_accepter.sh
+++ license_accepter.sh
@@ -22,6 +22,7 @@ accept_all_android_licenses() {
   fi
   accept_license_of android-sdk-license 8933bad161af4178b1185d1a37fbf41ea5269c55
   accept_license_of android-sdk-license d56f5187479451eabf01fb78af6dfcb131a6481e
+  accept_license_of android-sdk-license 24333f8a63b6825ea9c5514f83c2829b004d1fee
   accept_license_of android-sdk-preview-license 84831b9409646a918e30573bab4c9c91346d8abd
   accept_license_of intel-android-extra-license d975f751698a77b662f1254ddbeed3901e976f5a
 }
